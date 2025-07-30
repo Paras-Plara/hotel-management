b2606c9044d715e7d55de967ee06b74bb635c230
Paras Plara <paras.p@antino.com>
2025-07-30 13:32:41 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::middleware('auth')->group(function(){
     Route::get('/home', function () {
